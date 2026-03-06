0837c4bc24ffc2fccbf19dfa4ba75ba3eaa0ca91
Angel Rios <angelrios@Angels-MacBook-Air.local>
2026-03-06 15:24:42 -0500

    pre 2026 08

--- procesar_fact.py
+++ procesar_fact.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # ╚══════════════════════════════════════════════════════════════════════════════╝
 
 # ===== TABLA PRINCIPAL =====
-TABLA_PRINCIPAL = "EXT_CAMARON_2024_0.8.csv"
+TABLA_PRINCIPAL = "EXT_CAMARON_2026_0.8.csv"
 
 # ===== TABLAS DE SALIDA (12 tablas a llenar) =====
 TABLA_01_REGISTRO            = "Fact_Registro.csv"
@@ -37,7 +37,7 @@ DIM_MOTIVO_CAUSA    = "Dim_Motivo_Causa_No_Conforme.csv"
 DIM_DECISION_EMPLEO = "Dim_Decision_Empleo.csv"
 
 # ===== VARIABLES DE CONFIGURACIÓN =====
-Idrangolongitud   = [129, 130, 131, 132, 133, 134, 135, 136, 137]
+Idrangolongitud   = [138, 139, 140, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151]
 Idrangodiametro   = [152, 153, 154, 155, 156, 157, 158, 159, 160]
 Idvariablequimica = [1, 2, 3, 14, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
